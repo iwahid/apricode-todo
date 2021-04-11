b5484e8e1b4ffb6cebff5fef84044e2b6c0e7d8e
Iwahid <iwahid@yandex.ru>
2021-04-11 20:41:27 +0500

    Feat / Added new component (filter to select the desired todos list)

---
+++ src/components/TodosFilter/TodosFilter.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import storeTodos from '../../store/todos'
+import styles from './TodosFilter.module.scss'
+
+/**
+ * Component for filtering the todo list to display
+ * 
+ * @param {string} activeFilter - filter-flag (all, completed, unCompleted)
+ */
+export const TodosFilter = ({ activeFilter }) => {
+
+  const handleChangeFilter = (filter) => {
+    storeTodos.selectedFilter = filter
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <ul className={styles.filterList}>
+          <li>
+            <button
+              className={`${styles.filterList__item} ${activeFilter === 'all' && styles.filterList__item_active}`}
+              onClick={() => handleChangeFilter('all')}
+            >Все</button>
+          </li>
+          <li>
+            <button
+              className={`${styles.filterList__item} ${activeFilter === 'unCompleted' && styles.filterList__item_active}`}
+              onClick={() => handleChangeFilter('unCompleted')}
+            >Актуальные</button>
+          </li>
+          <li>
+            <button
+              className={`${styles.filterList__item} ${activeFilter === 'completed' && styles.filterList__item_active}`}
+              onClick={() => handleChangeFilter('completed')}
+            >Выполненые</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/components/СontentWrapper/СontentWrapper.jsx
+++ src/components/СontentWrapper/СontentWrapper.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import styles from './СontentWrapper.module.scss'
 
-/* Container for additional styling of current components */
+/**
+ * Container for additional styling of current components 
+ * 
+ * @param {Object} children - React components
+ */
 export const СontentWrapper = (props) => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
